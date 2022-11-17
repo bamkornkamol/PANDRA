466b0c3e91a949045d4eff84b79314083f604807
Bug9Best <Bug9Best@gmail.com>
2022-11-18 04:26:01 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { NavBarComponent } from './view/nav-bar/nav-bar.component';
+import { HomeComponent } from './view/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'pandora-c04db',
+    appId: '1:84666121571:web:271f5bec39d317305a2756',
+    storageBucket: 'pandora-c04db.appspot.com',
+    apiKey: 'AIzaSyCWAu5olFDasSsDwROgTZCmcmOe3_o4zfU',
+    authDomain: 'pandora-c04db.firebaseapp.com',
+    messagingSenderId: '84666121571',
+  },
+  production: true
+};
