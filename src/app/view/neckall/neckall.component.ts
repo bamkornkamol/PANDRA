860eeec216077287469173a36a94706b7a794d35
Bug9Best <Bug9Best@gmail.com>
2022-11-18 19:04:11 +0700

    get data from firebase all tab

--- src/app/view/neckall/neckall.component.ts
+++ src/app/view/neckall/neckall.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getDocs, Firestore, collection } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-neckall',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NeckallComponent implements OnInit {
 
-  constructor() { }
+  displayModal: boolean = false;
+
+  neckLaceList: any = [];
+  selectedProduct: any = [];
+
+  constructor(
+    private firestore: Firestore,
+  ) {
+    this.getNeckLace();
+   }
 
   ngOnInit(): void {
   }
 
+  getNeckLace() {
+    const firebase = collection(this.firestore, 'all_neck');
+    getDocs(firebase).then((response) => {
+      this.neckLaceList = [...response.docs.map((item) => {
+        return { ...item.data(), id: item.id }
+      })]
+    })
+  }
+
+  showModalDialog(data: any) {
+    this.displayModal = true;
+    this.selectedProduct = data;
+  }
 }
--- src/app/view/ringall/ringall.component.ts
+++ src/app/view/ringall/ringall.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getDocs, Firestore, collection } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-ringall',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RingallComponent implements OnInit {
 
-  constructor() { }
+  displayModal: boolean = false;
+
+  ringList: any = [];
+  selectedProduct: any = [];
+
+  constructor(
+    private firestore: Firestore,
+  ) {
+    this.getRing();
+   }
 
   ngOnInit(): void {
   }
 
+  getRing() {
+    const firebase = collection(this.firestore, 'all_ring');
+    getDocs(firebase).then((response) => {
+      this.ringList = [...response.docs.map((item) => {
+        return { ...item.data(), id: item.id }
+      })]
+    })
+  }
+
+  showModalDialog(data: any) {
+    this.displayModal = true;
+    this.selectedProduct = data;
+  }
 }
